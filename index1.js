8413b0ee0f601856ad162f23b4f6e592ec3a6154
Derrick Kay <82616888+derrickkay@users.noreply.github.com>
2022-01-19 20:03:59 +0000

    Create index1.js

---
+++ index1.js
@@ -0,0 +1,14 @@
+const myAge = 32; // My age
+let earlyYears = 2; // This is stored as 2
+earlyYears *= 10.5; // Times early Years by 10.5
+let laterYears = myAge - 2; // minues 2 from myAge 
+laterYears *= 4; // times 4 to laterYears
+let myAgeInDogYears = earlyYears + laterYears; // adds the 2 variables
+
+// console.log(earlyYears); 
+//console.log(laterYears); 
+//console.log(MyAgeInDogYears);
+
+let myName = 'Derrick Kay'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
